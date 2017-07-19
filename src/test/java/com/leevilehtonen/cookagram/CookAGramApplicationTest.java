2777ac29d7e734144bad41710ee1a34089e68c48
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-07-19 19:34:37 +0300

    travis

--- src/test/java/com/leevilehtonen/cookagram/CookAGramApplicationTest.java
+++ src/test/java/com/leevilehtonen/cookagram/CookAGramApplicationTest.java
@@ -4,10 +4,12 @@ package com.leevilehtonen.cookagram;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("development")
 public class CookAGramApplicationTest {
 
     @Test
