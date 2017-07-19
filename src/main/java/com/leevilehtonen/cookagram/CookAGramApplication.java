c69c5d710b7bfa925bd91dffbdaf89cc5f34ca81
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-07-19 16:20:06 +0300

    Hello world

---
+++ src/main/java/com/leevilehtonen/cookagram/CookAGramApplication.java
@@ -0,0 +1,13 @@
+package com.leevilehtonen.cookagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CookAGramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookAGramApplication.class, args);
+    }
+}
---
+++ src/main/java/com/leevilehtonen/cookagram/controller/DefaultController.java
@@ -0,0 +1,18 @@
+package com.leevilehtonen.cookagram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("*")
+public class DefaultController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public String getHome(Model model) {
+        return "Hello world";
+    }
+}
