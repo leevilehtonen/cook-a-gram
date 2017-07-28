7d25cf0f73f77f512490c67bbb47c4d2771d04b7
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-07-28 04:18:03 +0300

    Update image content type for Postgres

--- src/main/java/com/leevilehtonen/cookagram/config/PostgreSQLDialectCustom.java
+++ src/main/java/com/leevilehtonen/cookagram/config/PostgreSQLDialectCustom.java
@@ -1,18 +1,14 @@
 package com.leevilehtonen.cookagram.config;
 
 import org.hibernate.dialect.PostgreSQL94Dialect;
-import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
-import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
 import java.sql.Types;
 
 public class PostgreSQLDialectCustom extends PostgreSQL94Dialect {
 
-    @Override
-    public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
-        if (sqlTypeDescriptor.getSqlType() == Types.BLOB) {
-            return BinaryTypeDescriptor.INSTANCE;
-        }
-        return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
+    public PostgreSQLDialectCustom() {
+        registerColumnType(Types.BLOB, "bytea");
+        registerColumnType(Types.LONGVARBINARY, "bytea");
     }
+    
 }
